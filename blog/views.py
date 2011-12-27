77361b718e2d5b5bf9f2637e4d35a308974e7f06
alishiguang <alishiguang@gmail.com>
2011-12-27 11:31:28 +0800

    First commit

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+# Create your views here.
+from webblog.blog.models import *
+from django.shortcuts import render_to_response
+from django.template import Context, loader
+from django.http import HttpResponse
+
+def index(request):
+    article_list = Article.objects.all()
+    return render_to_response('index.html',{'article_list':article_list})
